6e368d22cfabf0ee8e0a703deecc99b276a56551
flatironschool <danliu277@gmail.com>
2020-08-03 17:11:35 -0400

    Update urls

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^api-auth/', include('rest_framework.urls')),
-    url(r'^admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('admin/', admin.site.urls),
 ]
