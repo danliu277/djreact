3b98fa61a8a716ab89e4e11aa774604cd837d36d
flatironschool <danliu277@gmail.com>
2020-08-11 16:36:37 -0400

    Default permission

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -128,9 +128,15 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+    ),
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
