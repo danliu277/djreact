1d78db1ded47e4c9a3cd22ab3f88f78642f7354d
flatironschool <danliu277@gmail.com>
2020-08-11 16:50:27 -0400

    Undo settings change causing get error

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -128,15 +128,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    # ]
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
