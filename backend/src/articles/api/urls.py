94aeee8076ffaa8ed00fac300bfd8ca0e98503a5
flatironschool <danliu277@gmail.com>
2020-08-12 17:07:32 -0400

    Convert api to viewsets

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,17 +1,24 @@
-from django.urls import path
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import (
-    ArticleListView, 
-    ArticleDetailView,
-    ArticleCreateView,
-    ArticleUpdateView,
-    ArticleDestroyView,
-)
+router = DefaultRouter()
+router.register(r'api', ArticleViewSet, basename='articles')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('articles/', ArticleListView.as_view()),
-    path('articles/create/', ArticleCreateView.as_view()),
-    path('articles/<pk>', ArticleDetailView.as_view()),
-    path('articles/<pk>/update/', ArticleUpdateView.as_view()),
-    path('articles/<pk>/delete/', ArticleDestroyView.as_view()),
-]
+# from django.urls import path
+
+# from .views import (
+#     ArticleListView, 
+#     ArticleDetailView,
+#     ArticleCreateView,
+#     ArticleUpdateView,
+#     ArticleDestroyView,
+# )
+
+# urlpatterns = [
+    # path('articles/', ArticleListView.as_view()),
+    # path('articles/create/', ArticleCreateView.as_view()),
+    # path('articles/<pk>', ArticleDetailView.as_view()),
+    # path('articles/<pk>/update/', ArticleUpdateView.as_view()),
+    # path('articles/<pk>/delete/', ArticleDestroyView.as_view()),
+# ]
