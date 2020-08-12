bb99e261d9bfe92eaf0d38516a5dad0a9e704cb0
flatironschool <danliu277@gmail.com>
2020-08-12 17:19:40 -0400

    Remove route and update api route

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -2,20 +2,26 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 import Articles from '../components/Article'
+import CustomForm from '../components/Form'
 
 const ArticleList = (props) => {
 
     const [articles, setArticles] = useState([])
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/articles')
+        axios.get('http://127.0.0.1:8000/api/')
             .then(res => {
                 setArticles(res.data)
             })
     }, [])
 
     return (
-        <Articles data={articles} />
+        <div>
+            <Articles data={articles} />
+            <br />
+            <h2>Create an Article</h2>
+            <CustomForm />
+        </div>
     );
 }
 
--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -2,14 +2,12 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import ArticleList from './containers/ArticleListView';
 import ArticleDetail from './containers/ArticleDetailView';
-import ArticleCreate from './containers/ArticleCreateView';
 
 const BaseRouter = () => {
     return (
         <div>
             <Route exact path='/' component={ArticleList} />
             <Route exact path='/:articleId' component={ArticleDetail} />
-            <Route exact path='/newArticle' component={ArticleCreate} />
         </div>
     )
 };
