4d91c147390f75356bcb2d378c5c14db270243ba
flatironschool <danliu277@gmail.com>
2020-08-12 17:38:30 -0400

    Update api routes

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -9,7 +9,7 @@ const ArticleList = (props) => {
     const [articles, setArticles] = useState([])
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/')
+        axios.get('http://localhost:8000/api/')
             .then(res => {
                 setArticles(res.data)
             })
