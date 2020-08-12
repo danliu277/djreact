04ac063fca9606d2242bed788cb41f5189a72f6b
flatironschool <danliu277@gmail.com>
2020-08-12 17:23:07 -0400

    Add submit handler

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -2,17 +2,27 @@ import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 
 const CustomForm = () => {
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+
+        const title = event.target.elements.title.value
+        const content = event.target.elements.content.value
+
+        console.log(title, content)
+    }
+
     return (
         <>
-            <Form>
+            <Form onSubmitCapture={handleFormSubmit}>
                 <Form.Item label="Title">
-                    <Input placeholder="Put a title here" />
+                    <Input name="title" placeholder="Put a title here" />
                 </Form.Item>
                 <Form.Item label="Content">
-                    <Input placeholder="Enter some content..." />
+                    <Input name="content" placeholder="Enter some content..." />
                 </Form.Item>
                 <Form.Item>
-                    <Button type="primary">Submit</Button>
+                    <Button type="primary" htmlType="submit" >Submit</Button>
                 </Form.Item>
             </Form>
         </>
