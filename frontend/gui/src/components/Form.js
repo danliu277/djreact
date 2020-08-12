45108397ece6ac3269d7aabd0b32feb4ee30e8c5
flatironschool <danliu277@gmail.com>
2020-08-12 17:30:13 -0400

    Add handleSubmit switch statements

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 
-const CustomForm = () => {
+import axios from 'axios'
 
-    const handleFormSubmit = (event) => {
+const CustomForm = (props) => {
+
+    const handleFormSubmit = (event, requestType, articleId) => {
         event.preventDefault()
 
         const title = event.target.elements.title.value
         const content = event.target.elements.content.value
 
-        console.log(title, content)
+        switch(requestType) {
+            case 'post':
+                axios.post(`http://localhost:8000/api/`, {
+                    title,
+                    content
+                }).then(res => console.log(res))
+                .catch(err => console.err(err))
+                break;
+            case 'put':
+                axios.put(`http://localhost:8000/api/${articleId}`, {
+                    title,
+                    content
+                }).then(res => console.log(res))
+                .catch(err => console.err(err))
+                break;
+            default:
+                console.log('default')
+        }
     }
 
     return (
         <>
-            <Form onSubmitCapture={handleFormSubmit}>
+            <Form onSubmitCapture={(event) => handleFormSubmit(event, props.requestType, props.articleId)}>
                 <Form.Item label="Title">
                     <Input name="title" placeholder="Put a title here" />
                 </Form.Item>
